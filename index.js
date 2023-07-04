9f47e7e3c63cde9c6e12443bdc328dcaa88290ee
Varshini11113 <varshini.m2021csec@sece.ac.in>
2023-07-04 09:32:27 +0530

    setInterval & setTimeout added

--- index.js
+++ index.js
@@ -59,10 +59,59 @@ let sampleFile=""
 
 
 //to make the data written in write file we use the func/- inside the read file
-fs.readFile("./sample.txt", function(err, data){
-    sampleFile = data;
-    fs.writeFile("./write.txt", "This write file is written by inbuilt function" , function(err){
-        console.log(err);
-    });
-    console.log(sampleFile.toString());
-});
+// fs.readFile("./sample.txt", function(err, data){
+//     sampleFile = data;
+//     fs.writeFile("./write.txt", "This write file is written by inbuilt function" , function(err){
+//         console.log(err);
+//     });
+//     console.log(sampleFile.toString());
+// });
+
+// fs.readFile("./sample.txt", function(err, data){
+//     sampleFile = data;
+//     fs.writeFile("./write.txt", "This write file is written by inbuilt function" , function(err){
+//         console.log(err);
+//     });
+//     console.log(sampleFile.toString());
+// });
+
+
+//.then tells the value is inserted (resolve)
+// new Promise(function(resolve, reject) {
+//     setTimeout (function() {
+//         resolve("Data")
+//     }, 2000)
+// }).then(function (val){
+//     console.log(val);
+// })
+
+//modern way of handling function
+// using async and await keywords instead then method with callback functions
+//async tells that this function takes some time to complete the job
+
+// async function test(){
+//     let fun = await fetch("");
+//     console.log(fun) 
+// } 
+// test();
+
+// setTimeout(function() {
+//     console.log("varshini");
+// }, 2000);
+
+// setInterval(function(){
+//     console.log("udhaya");
+// }, 1000);
+
+//to stop the setInterval
+
+var a = 10;
+setTimeout(function() {
+    if(a>1000)
+    clearInterval(interval);
+}, 4000);
+
+let interval = setInterval(function(){
+    a = a*10;
+    console.log(a);
+}, 1000);
